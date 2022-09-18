456397a60819ff151e7bc7b08d2220e13e7bfa53
hyunchul.jung <blackironj@gmail.com>
2022-09-18 17:47:37 +0900

    modified. logger init

--- util/logger/logger.go
+++ util/logger/logger.go
@@ -28,52 +28,6 @@ func Init() {
 	}
 
 	zerolog.SetGlobalLevel(logLevel)
-}
 
-func logEvent(level zerolog.Level, logFields ...map[string]interface{}) *zerolog.Event {
-	var e *zerolog.Event
-
-	switch level {
-	case zerolog.DebugLevel:
-		e = log.Debug()
-	case zerolog.InfoLevel:
-		e = log.Info()
-	case zerolog.WarnLevel:
-		e = log.Warn()
-	case zerolog.ErrorLevel:
-		e = log.Error()
-	case zerolog.FatalLevel:
-		e = log.Fatal()
-	case zerolog.PanicLevel:
-		e = log.Panic()
-	}
-
-	if len(logFields) > 0 {
-		e.Fields(logFields)
-	}
-	return e
-}
-
-func Debug(msg string, logFields ...map[string]interface{}) {
-	logEvent(zerolog.DebugLevel, logFields...).Msg(msg)
-}
-
-func Info(msg string, logFields ...map[string]interface{}) {
-	logEvent(zerolog.InfoLevel, logFields...).Msg(msg)
-}
-
-func Warn(msg string, logFields ...map[string]interface{}) {
-	logEvent(zerolog.WarnLevel, logFields...).Msg(msg)
-}
-
-func Error(msg string, logFields ...map[string]interface{}) {
-	logEvent(zerolog.ErrorLevel, logFields...).Msg(msg)
-}
-
-func Fatal(msg string, logFields ...map[string]interface{}) {
-	logEvent(zerolog.FatalLevel, logFields...).Msg(msg)
-}
-
-func Panic(msg string, logFields ...map[string]interface{}) {
-	logEvent(zerolog.PanicLevel, logFields...).Msg(msg)
+	log.Logger = log.With().Caller().Timestamp().Logger()
 }
