8f91f23a0fef5690c924367e02d4c7010fdbe323
hyunchul.jung <blackironj@gmail.com>
2022-10-10 16:16:59 +0900

    added. swagger example

--- main.go
+++ main.go
@@ -7,12 +7,18 @@ import (
 
 	"github.com/rs/zerolog/log"
 
+	_ "github.com/blackironj/rest-be-template/docs"
 	"github.com/blackironj/rest-be-template/env"
 	"github.com/blackironj/rest-be-template/repository"
 	"github.com/blackironj/rest-be-template/server"
 	"github.com/blackironj/rest-be-template/util/logger"
 )
 
+// @title rest-be-template Swagger Example
+// @version 0.1
+// @description This is a simple swagger
+// @host localhost:8080
+// @BasePath /
 func main() {
 	env.Init()
 	logger.Init()
